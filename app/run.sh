76ff159a41ce2d4dd0e76c53585d685ef990fb8a
bipin-a <aasibipin@gmail.com>
2024-12-03 09:59:08 -0500

    rm uv

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # path_to_your_main:app 
-uv run uvicorn app.main:app --host 'localhost' --port 8000 --reload
+uvicorn app.main:app --host 'localhost' --port 8000 --reload
 
 # This is using uvicorn 
 # Most people use Docker to run their FastAPI applications
